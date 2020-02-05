cb82b40931f49d50e4bad8fe2dd1cc0b7a94c211
Adron Hall <adronhall@gmail.com>
2020-02-04 16:10:30 -0800

    Fixed one thing, the other thing is still hit or miss.

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,60 @@
-'use strict'
+'use strict';
 
 const fs = require('fs');
 const path = './.data/secure-connect.zip';
 const { Client } = require('cassandra-driver');
+const client = new Client({
+  cloud: { secureConnectBundle: path },
+  credentials: { username: process.env.USERNAME, password: process.env.PASSWORD }
+});
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Better Botz Nous' });
+  res.render('index', { title: 'Better Botz Nous, Welcome' + process.env.USERNAME });
 });
 
 router.get('/data', function (req, res) {
-  var theData = 'Nope!';  
-  
-  var fileExists = fs.existsSync(path);
-  if (fileExists) {
-    theData = "The file exists.\n"
-    
-    theData += process.env.USERNAME
-    theData += process.env.PASSWORD  
-    
-    const client = new Client({
-      cloud: { secureConnectBundle: path },
-      credentials: { username: process.env.USERNAME, password: process.env.PASSWORD }
-    });
-    
-    client.connect();
+  getData().then(function(data){
+    res.send(data);
+  }).catch(function(filteredData){
+    res.send(filteredData);
+  })
+});
 
-    // Execute a query
-    const rs = client.execute('SELECT * FROM system.local');
-    theData += rs.first()['cluster_name']
-    
-    client.shutdown();
-  
-    res.send(theData);
-  } else {
-    res.send("Dammit!");
-  }
-})
+router.get('/metadata', function(req, res){
+  getMetadata().then(function (data) {
+    res.send(data);
+  }).catch(function (filteredData) {
+    res.send(filteredData);
+  })
+});
 
 async function getData() {
-  const client = new Client({
-    cloud: { secureConnectBundle: path },
-    credentials: { username: 'username', password: 'password' }
-  });
-
   await client.connect();
-
-  // Execute a query
   const rs = await client.execute('SELECT * FROM system.local');
-  console.log(`Hello from cluster: ${rs.first()['cluster_name']}`);
-
   await client.shutdown();
+  return rs.first()['cluster_name'];
+}
+
+async function getSampleRows() {
+
+}
+
+async function getMetadata() {
+  var result = "";
+  client.metadata.getTable('system', 'local')
+      .then(function (tableInfo) {
+        result += 'Table ' + tableInfo.name;
+        tableInfo.columns.forEach(function (column) {
+          result += 'Column ' + column.name + ' with type ' + column.type;
+        });
+      }).catch(function (filteredData) {
+      result = filteredData;
+  });
+  return "Data: " + result;
 }
 
 module.exports = router;
